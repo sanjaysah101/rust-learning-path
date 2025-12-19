2a623be3903ee4f94e8ebb1162ef618673ad4be4
Sanjay Kumar Sah <sanjay8797421521@gmail.com>
2025-12-19 22:22:16 +0530

    Completed Assignment 5: JSON Parser with recursive Enums

--- 05-json-parser/src/main.rs
+++ 05-json-parser/src/main.rs
@@ -1,3 +1,29 @@
+#[derive(Debug)]
+enum JsonValue {
+    Null,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+    Array(Vec<JsonValue>),
+    Object(Vec<(String, JsonValue)>),
+}
 fn main() {
-    println!("Hello, world!");
+    let complex_data = JsonValue::Object(vec![
+        // Simple Number
+        (String::from("user_id"), JsonValue::Number(101.0)),
+        // Object inside Object (Recursion)
+        (
+            String::from("profile"),
+            JsonValue::Object(vec![
+                (
+                    String::from("full_name"),
+                    JsonValue::String(String::from("Sanjay")),
+                ),
+                (String::from("active"), JsonValue::Boolean(true)),
+            ]),
+        ),
+    ]);
+
+    // {:#?} (the "pretty-print" formatter),
+    println!("{:#?}", complex_data);
 }
